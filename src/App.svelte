20704dc68841114efe6cec92663ad6d7261f92ae
Meddy <kaluumamhd@gmail.com>
2022-05-24 13:58:14 +0300

    Input data binding

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,32 @@
 	let name = "Meddy";
 	let country = "Uganda";
 	let district = "Kampala";
+	let color = "Black";
 
 	const changeDistrict = () => {
 		district = "Mpigi";
 	};
+
+	const changeColor = () => {
+		color = "Red";
+	};
+
+	// const inputChangeColor = (e) => {
+	// 	color = e.target.value;
+	// };
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>From: {country}</p>
 	<p>District: {district}</p>
+	<p style="color: {color};">Favourite color: {color}</p>
 
-	<button on:click={changeDistrict}>Change District</button>
+	<button on:click={changeDistrict}>Change District</button><br>
+	<button on:click={changeColor}>Change Fovourite Color</button>
+
+	<!-- <input type="text" on:input={inputChangeColor} value={color}> -->
+	<input type="text" placeholder="Enter Color" bind:value={color}>
 </main>
 
 <style>
